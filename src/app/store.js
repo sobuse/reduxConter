5a2f2f593f753f68fed46a38540a87ae0c5052c5
Ehueriken Samuel Obuse <59874300+sobuse@users.noreply.github.com>
2025-01-06 11:54:22 +0100

    Register the counter reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from '../features/counter/counterSlice'
 
-// configureStore creates a Redux store with good defaults
-// It automatically sets up Redux DevTools and some middleware
 export const store = configureStore({
   reducer: {
-    // This is where we'll register our reducers
-    // The key (counter) will be the name of the state slice
-    // counter: counterReducer
+    // Register the counter reducer
+    counter: counterReducer
   }
 })
