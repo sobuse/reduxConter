e5e532d08de41ab3216df1062fb8b054a56f9083
Ehueriken Samuel Obuse <59874300+sobuse@users.noreply.github.com>
2025-01-06 11:56:52 +0100

    made the Redux store available to alln nested components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux' // Import Provider
+import { store } from './app/store' // Import store
+import App from './App'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* Provider makes the Redux store available to any nested components */}
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 )
